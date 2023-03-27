779503d87d91e950c859a1f4fb4bc084e1610ace
ComboTWTW <gfakemail18@gmail.com>
2023-03-28 01:36:10 +0500

    animations created

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,8 +2,12 @@ import Navbar from "./Navbar"
 import { heroArrow, tab1, tab2, tab3, tab4 } from "../assets"
 import { Fade } from "react-awesome-reveal";
 import SubHero from "./SubHero";
+import { useState } from "react";
 
 const Hero = () => {
+
+  const [toggle, setToggle] = useState<boolean>(false);
+
   return (
     <div className="w-full bg-black text-center flex flex-col items-center overflow-hidden">
         
@@ -24,11 +28,13 @@ const Hero = () => {
             </div>
           </Fade>
 
+          <button onClick={() => setToggle(!toggle)} className='bg-white'>click me</button>
+
           <div className="flex flex-col items-center w-full mt-20 max-w-xs lg:flex-row md:justify-center lg:min-w-[1080px] md:mt-32 mb-24 lg:mb-44">
-            <img src={tab4} alt="tab4" className="z-[5] -rotate-3 lg:relative lg:max-w-[350px] lg:max-h-[100px] lg:left-44 lg:bottom-8"/>
-            <img src={tab3} alt="tab3" className="z-[4] -mt-3 rotate-3 lg:relative lg:max-w-[350px] lg:max-h-[100px] lg:top-12 lg:left-4"/>
-            <img src={tab2} alt="tab2" className="z-[3] -mt-4 -rotate-3 lg:relative lg:max-w-[350px] lg:max-h-[100px] lg:right-16"/>
-            <img src={tab1} alt="tab1" className="z-[2] -mt-3 rotate-3 lg:relative lg:max-w-[350px] lg:max-h-[100px] lg:right-40 lg:top-10"/>
+            <img src={tab4} alt="tab4" className={`z-[5] -rotate-3 ${toggle ? 'element1' : 'element11'} lg:relative lg:max-w-[350px] lg:max-h-[100px] lg:left-44 lg:bottom-8`}/>
+            <img src={tab3} alt="tab3" className={`z-[4] -mt-3 rotate-3 ${toggle ? 'element2' : 'element22'} lg:relative lg:max-w-[350px] lg:max-h-[100px] lg:top-12 lg:left-4`}/>
+            <img src={tab2} alt="tab2" className={`z-[3] -mt-4 -rotate-3 ${toggle ? 'element3' : 'element33'} lg:relative lg:max-w-[350px] lg:max-h-[100px] lg:right-16`}/>
+            <img src={tab1} alt="tab1" className={`z-[2] -mt-3 rotate-3 ${toggle ? 'element4' : 'element44'} lg:relative lg:max-w-[350px] lg:max-h-[100px] lg:right-40 lg:top-10`}/>
           </div> 
         </div>
         <SubHero />
